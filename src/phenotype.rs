eb2bfb35b63af42d6f257ba1fa40b2d0b76d929c
Constantin Mussaeus <cmussaeus@student.ethz.ch>
2022-09-08 14:11:16 +0200

    Documented phenotype and its functions

--- src/phenotype.rs
+++ src/phenotype.rs
@@ -4,15 +4,28 @@ use crate::NeuralNetwork;
 use rustc_hash::{FxHashMap, FxHasher};
 use std::hash::BuildHasherDefault;
 
+/// Graph representation of NeuralNetwork, used to compute its output.
 #[derive(Debug, PartialEq)]
 pub struct Phenotype {
+    /// EdgeList with the destination and the weight of each edge for each node.
     edges: Vec<Vec<(usize, f32)>>,
+    /// Array used to store and mutate the values of each node.
     pub node_value_array: Vec<f32>,
+    /// Order in which the nodes need to be processed such that all predecessors of a node have
+    /// been processed before it is being processed itself.
     pub topo_order: Vec<usize>,
+    /// List of indexes of the outputs of the network.
     outputs: Vec<usize>,
 }
 
 impl Phenotype {
+    /// Construct a Phenotype from a NeuralNetwork.
+    /// At first create an index mapping so that you know which NodeId you need to map to which
+    /// index. For this a fast FxHashMap is being used.
+    ///
+    /// Afterwards initialize an empty EdgeList and construct the outputs index array.
+    /// Then populating the EdgeList with the correct values using the index mapping.
+    /// At last creating the node value array and computing the topological order.
     pub fn from_nn(nn: &NeuralNetwork) -> Self {
         let input_length = nn.size.0;
         let output_length = nn.size.1;
@@ -46,6 +59,8 @@ impl Phenotype {
         }
     }
 
+    /// Function to create the topological order for computation of the network without any
+    /// uncomputed predecessors. Using DFS to create the order.
     fn create_topo_order(edges: &[Vec<(usize, f32)>], input_nodes: usize) -> Vec<usize> {
         let mut stack: Vec<usize> = Vec::new();
         // add every input node (one more than input bc of the constant) to the stack for dfs
@@ -79,6 +94,7 @@ impl Phenotype {
         order
     }
 
+    /// Creating the node index mapping using a simple and very fast hashmap.
     fn create_node_index_mapping(nodes: &[Node]) -> FxHashMap<usize, usize> {
         let mut map: FxHashMap<usize, usize> = FxHashMap::with_capacity_and_hasher(
             nodes.len(),
@@ -90,6 +106,11 @@ impl Phenotype {
         map
     }
 
+    /// Computing the output of the network depending on the input values.
+    ///
+    /// At first filling the node values, then traversing the network in topological order.
+    /// For each node in the beginning calculate the sigmoid value of its own value and then for
+    /// each edge of that node add the edge weight times the node's value to the destination node.
     pub fn compute(&mut self, inputs: Vec<f32>) -> Vec<f32> {
         let mut outputs: Vec<f32> = Vec::with_capacity(self.outputs.len());
         self.node_value_array.push(1.0);
@@ -112,12 +133,15 @@ impl Phenotype {
         outputs
     }
 
+    /// Reset the phenotype for reused computation
     pub fn reset(&mut self) {
         self.node_value_array.clear();
     }
 }
 
 // we can also choose another activation function
+/// Sigmoid activation function
+/// Approximated by x/(1+|x|)
 pub fn sigmoid(x: f32) -> f32 {
     x / (1.0 + x.abs())
 }
